59f01aa4d569cb96b7783ad5d9fccd6a863e6bd6
sofia barros <barrosbedoyasofia@gmail.com>
2023-03-17 20:42:22 -0500

    Se crea index

---
+++ src/AcercaDe/AcercaDe.js
@@ -0,0 +1,20 @@
+export function AcercaDe() {
+    return(
+        <>
+            <section>
+                <div class="container my-5">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-md-4">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/bybright-efa55.appspot.com/o/%E2%80%94Pngtree%E2%80%94yellow%20handbag%20beautiful%20handbag%20ladies_3874921.png?alt=media&token=574cccc2-9369-4baa-b9d7-798cf6e22698" alt="foto" class="img-fluid"/>
+                        </div>
+                        <div class="col-12 col-md-4 text-center align-self-center">
+                            <h2>byBright</h2>
+                            <p>PONGO AQUÍ LA DESCRIPCION</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+        
+    )
+}
---
+++ src/Servicios/Servicios.js
@@ -0,0 +1,24 @@
+import './Servicios.css'
+export function Servicios() {
+    
+    return(
+        <>
+            <section class="container-fluid my-5">
+                <div class="row text-center justify-content-around">
+                    <div class="col-12 col-md-3 fondo">
+                        <h2>Servicio 1</h2>
+                        <i class="bi bi-credit-card-2-front-fill fs-1"></i>
+                    </div>
+                    <div class="col-12 col-md-3 fondo">
+                    <h2>Servicio 1</h2>
+                        <i class="bi bi-credit-card-2-front-fill fs-1"></i>
+                    </div>
+                    <div class="col-12 col-md-3 fondo">
+                    <h2>Servicio 1</h2>
+                        <i class="bi bi-credit-card-2-front-fill fs-1"></i>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
